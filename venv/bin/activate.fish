63d53d74ec2898f158ed095d7dc134e91b068336
Arthur <arthurdevcontatoo@gmail.com>
2025-07-18 11:56:53 -0300

    Configurando css

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/arthur/taskmanager/venv
+set -gx VIRTUAL_ENV /home/arthur/task-manager/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
