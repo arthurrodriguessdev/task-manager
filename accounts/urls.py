7c804e1d5c9361ec19d212dfe17537184c0de1fa
Arthur <arthurdev.contatoo@gmail.com>
2025-07-21 19:19:05 -0300

    implementando autenticacao nos formularios de login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from accounts.views import login_view
+from accounts.views import login_view, cadastro_view
 
 urlpatterns = [
-    path('', login_view, name="login_view")
+    path('', login_view, name="login_view"),
+    path('cadastro/', cadastro_view, name="cadastro_view"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User # venv > lib64 > django > contrib > auth > models.py > User(Abstract)...
+from django.contrib.auth import authenticate, login
 
+#Criando um usuário
+def cadastro_view(request):
+    if request.method == 'GET': #Se o usuário SOMENTE ACESSOU a página
+        return render(request, 'accounts/cadastro.html')
+    else: #Senão for GET, é POST, usuário enviou os dados, clicou no botão Cadastrar
+        username = request.POST.get('nome') #Recebendo os dados
+        senha = request.POST.get('senha') #Recebendo os dados
+
+        user = User.objects.filter(username=username).first() #Se já tiver esse usuário no banco, armazena na variável user
+
+        if user:
+            return HttpResponse('Já existe um usuário com esse nome')
+        else:
+            user = User.objects.create_user(username=username, password=senha)
+            return redirect('login_view')
+
+#Entrando, logando com esse usuário
 def login_view(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'GET':
+        return render(request, 'accounts/login.html')
+    else:
+        username = request.POST.get('nome') #Recebendo os dados
+        senha = request.POST.get('senha') #Recebendo os dados
+
+        user = authenticate(username=username, password=senha) #TENTA autenticar com os dados fornecidos
+
+        if user:
+            login(request, user)
+            return HttpResponse('Autenticado')
+        else:
+            return HttpResponse('E-mail ou senha inválidos')
+        
